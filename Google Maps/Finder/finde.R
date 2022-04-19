59fc47245a356e9619553f61d38f4269657a802f
ikanx101 <ikanx101@gmail.com>
2022-04-19 11:42:04 +0700

    google maps finder

--- Google Maps/Finder/finde.R
+++ Google Maps/Finder/finde.R
@@ -1,11 +1,23 @@
+# ==============================================================================
+#
+# GOOGLE MAPS FINDER
+# by: ikanx101.com
+# digunakan untuk mencari tempat di google maps
+#
+# ==============================================================================
+
+# set working directory
 setwd("~/Documents/Sumut_Graber/Google Maps/Finder")
 
+# dimulai dari hati yang bersih
 rm(list=ls())
 
+# memanggil semua libraries yang terlibat
 library(dplyr)
 library(rvest)
 library(RSelenium)
 
+# function I
 # proses utk scrape link tempat
 ambil_link = function(){
   # kita ambil linknya dulu
@@ -20,6 +32,7 @@ ambil_link = function(){
   return(links[1])
 }
 
+# function II
 # ini adalah function untuk scrape info dari google maps
 scrape_google_maps = function(url){
   remote_driver$navigate(url)
@@ -43,33 +56,43 @@ scrape_google_maps = function(url){
     strsplit(split = "Ringkasan ulasan diberikan oleh") %>% 
     unlist() %>% 
     .[2] %>% 
+    stringr::str_squish() %>% 
+    strsplit(split = "TrustYou. 5 4 3 2 1") %>% 
+    unlist() %>% 
+    .[2] %>% 
     strsplit(split = "Orang lain juga menelusuri") %>% 
     unlist() %>% 
-    .[1] %>% 
-    stringr::str_squish()
+    .[1]
   output$review = review
   output$url = url
   return(output)
 }
 
-
+# function III
+# proses untuk scrape semua informasi
+# function paling ultimate
+cari_tempat = function(nama_tempat){
+  # set url
+  url = "https://www.google.co.id/maps/"
+  # membuka situs google maps
+  remote_driver$navigate(url)
+  # cari maps
+  cari_maps = remote_driver$findElement(using = 'css', value = '#searchboxinput')
+  cari_maps$sendKeysToElement(list(nama_tempat,key = "enter"))
+  # ambil hasil pencarian teratas
+  url_1 = ambil_link()
+  # kalau ada hasilnya
+  if(length(url) == 1){
+    tes = scrape_google_maps(url_1)
+  }
+  # jika tidak ada hasilnya
+  if(length(url) != 1){tes = NA}
+  # return hasil function
+  return(tes)
+}
 
 # memulai selenium
 driver =  RSelenium::rsDriver(browser = "chrome",
                               chromever = "99.0.4844.35" )
 remote_driver = driver[["client"]] 
-
-# set url
-url = "https://www.google.co.id/maps/"
-
-# membuka situs google maps
-remote_driver$navigate(url)
-
-cari_produk = remote_driver$findElement(using = 'css', value = '#searchboxinput')
-cari_produk$sendKeysToElement(list("mushalla nurul hikmah bekasi jaya",key = "enter"))
-
-
-
-url_1 = ambil_link()
-tes = scrape_google_maps(url_1)
 
