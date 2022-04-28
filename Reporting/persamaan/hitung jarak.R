e5b60c52aac5aa96db504eeec5549dd4bd18c37b
ikanx101 <ikanx101@gmail.com>
2022-04-28 10:53:30 +0700

    updates

--- Reporting/persamaan/hitung jarak.R
+++ Reporting/persamaan/hitung jarak.R
@@ -1,14 +1,14 @@
-# dimulai dari hati yang bersih
-rm(list=ls())
+# functions ini untuk di-run sekaligus
 
 # load libraries
-library(dplyr)
 library(stringdist)
 
 # fungsi untuk menghitung simmilarity
 kesamaan = function(a,b){
-  jarak = stringdist(a,b,method = "jaccard")
-  return(jarak)
+  jarak_1 = stringdist(a,b,method = "jaccard")
+  jarak_2 = stringdist(a,b,method = "cosine")
+  output = mean(jarak_1,jarak_2)
+  return(output)
 }
 
 # fungsi untuk membersihkan string
@@ -18,26 +18,3 @@ bersihin = function(text){
     janitor::make_clean_names() %>% 
     gsub("\\_"," ",.)
 }
-
-# import data
-load("~/Documents/Sumut_Graber/Reporting/pre processing/grab.rda")
-load("~/Documents/Sumut_Graber/Reporting/pre processing/gmaps.rda")
-
-# untuk binjai
-df_grab_binjai = df_grab %>% filter(grepl("pematang",kota,ignore.case = T))
-df_maps_binjai = df_maps %>% filter(grepl("pematang",kota,ignore.case = T))
-
-df_similarity = 
-  expand.grid(df_grab_binjai$nama_resto %>% bersihin() %>% gsub("pematang siantar","",.),
-              df_maps_binjai$nama_tempat %>% bersihin() %>% gsub("pematang siantar","",.)
-              ) %>% 
-  rename(grab = Var1,
-         maps = Var2) %>% 
-  rowwise() %>% 
-  mutate(simi = kesamaan(grab,maps)) %>% 
-  ungroup()
-
-df_similarity %>% 
-  group_by(grab) %>% 
-  filter(simi <= .1) %>% 
-  ungroup() %>% View()
